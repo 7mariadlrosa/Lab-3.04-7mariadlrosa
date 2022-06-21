70b7d28900a303409e26a5c827f3d4cef6f58819
María Fernández de la Rosa <100228799+7mariadlrosa@users.noreply.github.com>
2022-06-21 11:11:32 +0200

    .

--- src/main/java/com/ironhack/Lab3_04/repository/AirlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.Lab3_04.repository;
-import com.ironhack.Lab3_04.model.Airline;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AirlineRepository extends JpaRepository<Airline, Integer> {
-
-}
